e711b03bdd913a5e363397b89c53bc0385402b27
Assistant-C <besinger54163@gmail.com>
2025-06-19 23:30:10 +0300

    first commit5

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import WOW from 'wowjs';
 import 'animate.css';
 import work1 from './assets/work1.png';
 import work2 from './assets/work2.png';
-import work3 from './assets/work3.png';
+import work3 from './assets/work3.PNG';
 import work4 from './assets/work4.png';
 import work5 from './assets/work5.png';
 import photo from './assets/photo.jpg';
